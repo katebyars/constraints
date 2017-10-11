ad5e6a73468b54ac09bddd70bfb3f95b22a0a284
Kate Byars <katebyars5@gmail.com>
2017-10-11 16:03:44 -0700

    add all files.

---
+++ app/src/main/java/mymadlibs/epicodus/com/constraits/MainActivity.java
@@ -0,0 +1,13 @@
+package mymadlibs.epicodus.com.constraits;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
